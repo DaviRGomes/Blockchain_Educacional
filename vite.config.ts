a8eed778690edcd1e787b21251d114540f8ddb3b
Davi Rosa Gomes <126982048+DaviRGomes@users.noreply.github.com>
2025-12-06 15:16:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Blockchain_Educacional/',
   plugins: [react()],
   test: {
     globals: true,
